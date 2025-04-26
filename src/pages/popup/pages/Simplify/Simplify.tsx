ab98313244d491fcc02d8870060af3a849626e0d
y.mihalev <y.mihalev@vkteam.ru>
2025-04-26 15:25:12 +0300

    feat: Сделал переключение режима работы упрощения текста через выпадающий список

--- src/pages/popup/pages/Simplify/Simplify.tsx
+++ src/pages/popup/pages/Simplify/Simplify.tsx
@@ -5,6 +5,7 @@ import InfoBlockSimplify from "@pages/popup/components/InfoBlockSimplify/InfoBlo
 import {T_SimplifyState, T_Stats} from "@src/types";
 import {animated, useSpring} from "react-spring";
 import {DEFAULT_SIMPLIFY_STATE} from "@src/state";
+import {Dropdown} from "@pages/popup/components/Dropdown/Dropdown";
 
 export const Simplify = () => {
 	const [state, setState] = useState<T_SimplifyState>(null);
@@ -12,13 +13,10 @@ export const Simplify = () => {
 	const [stats, setStats] = useState<T_Stats>(null);
 
 	useEffect(() => {
-		chrome.storage.sync.get<T_SimplifyState>(
-			DEFAULT_SIMPLIFY_STATE,
-			(state) => {
-				setState(state);
-				setStats(state.simplifyStats);
-			},
-		);
+		chrome.storage.sync.get<T_SimplifyState>(DEFAULT_SIMPLIFY_STATE, (state) => {
+			setState(state);
+			setStats(state.simplifyStats);
+		});
 
 		chrome.storage.onChanged.addListener((updatedState) => {
 			if ("simplifyStats" in updatedState) {
@@ -30,24 +28,11 @@ export const Simplify = () => {
 	const handleToggleSimplifyEnabled = () => {
 		chrome.storage.sync.set({
 			simplifyEnabled: !state.simplifyEnabled,
-			simplifyDynamic: !state.simplifyEnabled,
 		});
 
 		setState({
 			...state,
 			simplifyEnabled: !state.simplifyEnabled,
-			simplifyDynamic: !state.simplifyEnabled,
-		});
-	};
-
-	const handleToggleSimplifyDynamic = () => {
-		chrome.storage.sync.set({
-			simplifyDynamic: !state.simplifyDynamic,
-		});
-
-		setState({
-			...state,
-			simplifyDynamic: !state.simplifyDynamic,
 		});
 	};
 
@@ -56,12 +41,9 @@ export const Simplify = () => {
 	const props = useSpring({
 		delay: 50,
 		from: {
-			height:
-				!state?.simplifyEnabled || ref.current
-					? "0px"
-					: "75px",
+			height: !state?.simplifyEnabled || ref.current ? "0px" : "175px",
 		},
-		to: {height: state?.simplifyEnabled ? "75px" : "0px"},
+		to: {height: state?.simplifyEnabled ? "175px" : "0px"},
 	});
 
 	if (!state) {
@@ -72,63 +54,39 @@ export const Simplify = () => {
 		<div className="flex flex-col gap-8 justify-between w-full h-full">
 			<div>
 				<div className="flex justify-between items-center mb-4">
-					<h1 className="text-stone-900 text-base font-black">
-						Упрощение текста
-					</h1>
-					<Toggle
-						bg="dark"
-						value={state.simplifyEnabled}
-						setValue={
-							handleToggleSimplifyEnabled
-						}
-					/>
+					<h1 className="text-stone-900 text-base font-black">Упрощение текста</h1>
+					<Toggle bg="dark" value={state.simplifyEnabled} setValue={handleToggleSimplifyEnabled} />
 				</div>
-				<animated.nav
-					style={props}
-					ref={ref}
-					className="pl-4 flex flex-col gap-4 overflow-hidden"
-				>
+				<animated.nav style={props} ref={ref} className="pl-4 flex flex-col gap-4 overflow-hidden">
 					<div className="flex justify-between items-center">
-						<span className="text-stone-900 text-base">
-							На всей странице
-						</span>
-						<Toggle
-							bg="dark"
-							value={
-								state.simplifyDynamic
-							}
-							setValue={
-								handleToggleSimplifyDynamic
-							}
+						<span className="text-stone-900 text-base">Режим работы</span>
+						<Dropdown
+							values={["На всей странице", "В выделенном тексте"]}
+							selectedValue={state.simplifyDynamic ? "На всей странице" : "В выделенном тексте"}
+							setValue={(value) => {
+								chrome.storage.sync.set({
+									simplifyDynamic: value == "На всей странице",
+								});
+
+								setState({...state, simplifyDynamic: value == "На всей странице"});
+							}}
 						/>
 					</div>
-					<div className="flex justify-between items-center">
-						<span className="text-stone-900 text-base">
-							В выделенном тексте
-						</span>
-						<div className="text-base">
-							<KeyboardTooltip
-								keys={["G"]}
-							/>
+					{!state.simplifyDynamic && (
+						<div className="flex justify-between items-center">
+							<span className="text-stone-900 text-base">Горячая клавиша</span>
+							<div className="text-base">
+								<KeyboardTooltip keys={["G"]} />
+							</div>
 						</div>
-					</div>
+					)}
 				</animated.nav>
 			</div>
-			{state.simplifyEnabled &&
-				stats &&
-				stats.wordsReplaced > 0 &&
-				stats.wordsAnalyzed > 0 && (
-					<div className="flex w-full justify-center">
-						<InfoBlockSimplify
-							replaced={
-								stats.wordsReplaced
-							}
-							total={
-								stats.wordsAnalyzed
-							}
-						/>
-					</div>
-				)}
+			{state.simplifyEnabled && stats && stats.wordsReplaced > 0 && stats.wordsAnalyzed > 0 && (
+				<div className="flex w-full justify-center">
+					<InfoBlockSimplify replaced={stats.wordsReplaced} total={stats.wordsAnalyzed} />
+				</div>
+			)}
 		</div>
 	);
 };
