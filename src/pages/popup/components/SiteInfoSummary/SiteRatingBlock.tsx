3363ebb47c81764657b0c93a9b4b7f1cfb4d7891
YarikMix <43493788+YarikMix@users.noreply.github.com>
2025-05-16 22:41:18 +0300

    feat: Рейтинг сайта (интеграция с бэкендом)

--- src/pages/popup/components/SiteInfoSummary/SiteRatingBlock.tsx
+++ src/pages/popup/components/SiteInfoSummary/SiteRatingBlock.tsx
@@ -2,12 +2,43 @@ import styles from "./SiteRatingBlock.module.css";
 import {useSiteDomen} from "@pages/popup/hooks/useSiteDomen";
 import clsx from "clsx";
 import ExternalLink from "@pages/popup/components/Link/Link";
-import {LANDING_URL} from "@src/utils/consts";
+import {API_URL, LANDING_URL} from "@src/utils/consts";
+import axios from "axios";
+import {useEffect, useState} from "react";
 
 const SiteRatingBlock = () => {
 	const {siteDomen} = useSiteDomen();
 
-	const place = 1;
+	const [place, setPlace] = useState(null);
+
+	const fetchSiteRating = async (domen) => {
+		try {
+			const response = await axios.get(API_URL + `/get_site_rating?site=${domen}`);
+
+			setPlace(response.data.place);
+		} catch {
+			setPlace(null);
+		}
+	};
+
+	useEffect(() => {
+		if (siteDomen) {
+			fetchSiteRating(siteDomen);
+		}
+	}, [siteDomen]);
+
+	if (!place) {
+		return (
+			<div className={clsx(styles.container, "bg-gray-800", "shadow-md")}>
+				<div className={styles.rightContainer}>
+					<span className={styles.label}>
+						Для анализа рейтинга ресурса <b>{siteDomen}</b> еще недостаточно данных
+					</span>
+					<ExternalLink label="Открыть общий рейтинг" url={LANDING_URL} />
+				</div>
+			</div>
+		);
+	}
 
 	return (
 		<div className={clsx(styles.container, "bg-gray-800", "shadow-md")}>
